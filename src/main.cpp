5ab268d8cac71b3d813c9ba1e6600469660ddba7
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-08-01 11:55:37 -0300

    Add main file
    Main file with basic app
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("KDE");
+    QCoreApplication::setOrganizationDomain("kde.org");
+    QCoreApplication::setApplicationName("KPS - KDE Print Service");
+    
+    return app.exec();
+}
