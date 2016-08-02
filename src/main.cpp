b90e98effed145151c7b970f5b25c41575bc4bdf
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-08-02 15:36:09 -0300

    Change class to use a Ui file
    Write the code to be able to manage the Ui for the
    mainWindow.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        qDebug() << "Please use: kps port baudrate. (./kpg /dev/ttyUSB0 115200)";
-        return -1;
-    }
-
     QApplication app(argc, argv);
 
     QCoreApplication::setOrganizationName("KDE");
     QCoreApplication::setOrganizationDomain("kde.org");
     QCoreApplication::setApplicationName("KPS - KDE Print Service");
 
-    MainWindow *window = new MainWindow(argv[1], atoi(argv[2]));
+    MainWindow window;
+    window.show();
+
     return app.exec();
 }
