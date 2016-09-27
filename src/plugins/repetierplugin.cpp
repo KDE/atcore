b8579dcc07f0eac62d4fd5a1cb2ccd4c3e09e664
Patrick José Pereira <patrickelectric@gmail.com>
2016-09-26 21:41:30 -0300

    repetierplugin: Add const QStrings for contains
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/plugins/repetierplugin.cpp
+++ src/plugins/repetierplugin.cpp
@@ -2,6 +2,11 @@
 #include <QLoggingCategory>
 #include <QString>
 
+QString RepetierPlugin::_ok = QStringLiteral("ok");
+QString RepetierPlugin::_wait = QStringLiteral("wait");
+QString RepetierPlugin::_extruderTemp = QStringLiteral("T:");
+QString RepetierPlugin::_bedTemp = QStringLiteral("B:");
+
 Q_LOGGING_CATEGORY(REPETIER_PLUGIN, "org.kde.atelier.core.firmware.repetier");
 
 QString RepetierPlugin::name() const
@@ -16,7 +21,7 @@ RepetierPlugin::RepetierPlugin(QObject *parent)
 
 bool RepetierPlugin::readyForNextCommand(const QString &lastMessage)
 {
-    if (lastMessage.contains("ok") || lastMessage.contains("wait")) {
+    if (lastMessage.contains(_ok) || lastMessage.contains(_wait)) {
         return true;
     }
     return false;
--- src/plugins/repetierplugin.h
+++ src/plugins/repetierplugin.h
@@ -9,6 +9,11 @@ class RepetierPlugin : public QObject, public IFirmware
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.kde.atelier.core.firmware" FILE "repetier.json")
     Q_INTERFACES(IFirmware)
+private:
+    static QString _ok;
+    static QString _wait;
+    static QString _extruderTemp;
+    static QString _bedTemp;
 public:
     RepetierPlugin(QObject *parent = 0);
     QString name() const override;
