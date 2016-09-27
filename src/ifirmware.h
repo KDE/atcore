e3d5a60ab3ad5105ac163be560c8479e5b571752
Patrick José Pereira <patrickelectric@gmail.com>
2016-09-26 21:19:15 -0300

    ifirmware: Add printer status struct
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/ifirmware.h
+++ src/ifirmware.h
@@ -10,6 +10,14 @@ public:
     virtual QString name() const = 0;
     virtual bool readyForNextCommand(const QString &lastMessage) = 0;
     // ADD THE METHODS HERE.
+
+    struct {
+        //TODO: expand extruderTemps to list for multiple extruders
+        float extruderTemp;
+        float extruderTargetTemp;
+        float bedTemp;
+        float bedTargetTemp;
+    } PrinterStatus;
 };
 
 Q_DECLARE_INTERFACE(IFirmware, "org.kde.atelier.core.firmware")
