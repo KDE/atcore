ad73d69549ca6297d674cb108f23109ea1391db6
Chris Rizzitello <sithlord48@gmail.com>
2017-07-13 00:06:00 -0400

    Add Messages.sh file
    Signed-off-by: Chris Rizzitello <sithlord48@gmail.com>

---
+++ Messages.sh
@@ -0,0 +1,4 @@
+ #! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
+$EXTRACT_TR_STRINGS `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/atcore_qt.po
+
