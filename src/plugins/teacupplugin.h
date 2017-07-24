d2168532beb6501c1ce98023065912eb9f130c21
Chris Rizzitello <rizzitello@kde.org>
2017-07-23 20:55:47 -0400

    Update Documentation for TeacupPlugin
    Signed-off-by: Chris Rizzitello <rizzitello@kde.org>

--- src/plugins/teacupplugin.h
+++ src/plugins/teacupplugin.h
@@ -26,17 +26,44 @@
 
 #include "ifirmware.h"
 #include <QObject>
-
+/**
+ * @brief The TeacupPlugin class
+ * Plugin for Teacup
+ */
 class TeacupPlugin : public IFirmware
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.kde.atelier.core.firmware" FILE "teacup.json")
     Q_INTERFACES(IFirmware)
+
 private:
+    /**
+     * @brief command finished string
+     */
     static QString _ok;
+
 public:
+    /**
+     * @brief Create new TeacupPlugin
+     */
     TeacupPlugin();
+
+    /**
+     * @brief Return Plugin name
+     * @return Teacup
+     */
     QString name() const override;
+
+    /**
+     * @brief Check if command contains TeacupPlugin::_ok
+     * @param lastMessage: last message from printer
+     */
     void validateCommand(const QString &lastMessage) override;
+
+    /**
+     * @brief Translate common commands to firmware specific command.
+     * @param command: command to translate
+     * @return firmware specific translated command
+     */
     QByteArray translate(const QString &command) override;
 };
