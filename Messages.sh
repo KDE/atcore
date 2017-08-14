aeb04a0d40dda0d614e6647aaf4d21ede56eb4fd
Pino Toscano <pino@kde.org>
2017-08-14 21:20:51 +0200

    i18n: do not extract messages from 'unittests' subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
  #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$EXTRACT_TR_STRINGS `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/atcore_qt.pot
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg | grep -v '/unittests/'` >> rc.cpp
+$EXTRACT_TR_STRINGS `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml | grep -v '/unittests/'` -o $podir/atcore_qt.pot
 
