87148a3c4c23eef15f9fd62f5ed4ffe621f4ba81
Chris Rizzitello <rizzitello@kde.org>
2018-03-28 15:36:01 -0400

    Tomaz' and Lays' Changes  - Remove static vars to allow for more then one instance.  - Clean up lots of logic  - Add uncredited authors

--- testclient/widgets/plotwidget.h
+++ testclient/widgets/plotwidget.h
@@ -1,14 +1,14 @@
 /* Atelier KDE Printer Host for 3D Printing
     Copyright (C) <2016>
-    Author: Patrick José Pereira - patrickelectric@gmail.com
+     Author: Patrick José Pereira - patrickjp@kde.org
+             Tomaz Canabrava <tcanabrava@kde.org>
+             Lays Rodrigues <lays.rodrigues@kde.org>
+             Chris Rizzitello <rizzitello@kde.org>
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 3 of
-    the License or any later version accepted by the membership of
-    KDE e.V. (or its successor approved by the membership of KDE
-    e.V.), which shall act as a proxy defined in Section 14 of
-    version 3 of the license.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -31,21 +31,21 @@ class PlotWidget : public QWidget
 
 public:
     explicit PlotWidget(QWidget *parent = nullptr);
-    ~PlotWidget() override;
+    ~PlotWidget();
 
     /**
      * @brief Create a new plot
      *
      * @param name p_name: plot name
      */
-    void newPlot(QString name);
+    void newPlot(const QString &name);
 
     /**
      * @brief Delete plot with name
      *
      * @param name p_name: name
      */
-    void deletePlot(QString name);
+    void deletePlot(const QString &name);
 
     /**
      * @brief Append point to plot
@@ -53,7 +53,7 @@ public:
      * @param name p_name: plot name
      * @param value p_value: value
      */
-    void appendPoint(QString name, float value);
+    void appendPoint(const QString &name, float value);
 
     /**
      * @brief Update plot list, need to run after ALL plots added
@@ -61,17 +61,11 @@ public:
      */
     void update();
 private:
-
-    QChartView *_chart;
-
-    QHash<QString, int> _name2Index;
-
     class plot
     {
     public:
-        explicit plot()
+        explicit plot() : _series(new QLineSeries())
         {
-            _series = new QLineSeries();
         };
 
         ~plot()
@@ -85,7 +79,7 @@ private:
             _series->append(now.toMSecsSinceEpoch(), value);
         }
 
-        void setName(QString name)
+        void setName(const QString &name)
         {
             _name = name;
             _series->setName(_name);
@@ -97,12 +91,12 @@ private:
             _series->append(now.toMSecsSinceEpoch(), 0.0);
         }
 
-        QLineSeries *serie()
+        QLineSeries *serie() const
         {
             return _series;
         }
 
-        QString name()
+        QString name() const
         {
             return _name;
         }
@@ -111,6 +105,10 @@ private:
         QLineSeries *_series;
         QString _name;
     };
+    QChartView *_chart;
+    QDateTimeAxis *_axisX;
+    QValueAxis *_axisY;
 
+    QHash<QString, int> _name2Index;
     QVector<plot> _plots;
 };
