cead77de77ca2fc924d294c840de5ab384286076
Sabyasachi Sahoo <Sahoosabyasachi07@gmail.com>
2021-04-16 19:32:03 +0530

    Changing Initial value of Speed and Flow rate

--- src/core/atcore.cpp
+++ src/core/atcore.cpp
@@ -654,7 +654,7 @@ void AtCore::setBedTemp(uint temp, bool andWait)
 
 void AtCore::setFanSpeed(uint speed, uint fanNumber)
 {
-    speed = std::max<uint>(1, std::min<uint>(speed, 10000));
+    speed = std::max<uint>(0, std::min<uint>(speed, 10000));
     fanNumber = std::min<uint>(fanNumber, 10000);
 
     pushCommand(GCode::toCommand(GCode::MCommands::M106, QString::number(fanNumber), QString::number(speed)));
@@ -662,13 +662,13 @@ void AtCore::setFanSpeed(uint speed, uint fanNumber)
 
 void AtCore::setPrinterSpeed(uint speed)
 {
-    speed = std::max<uint>(1, std::min<uint>(speed, 10000));
+    speed = std::max<uint>(0, std::min<uint>(speed, 10000));
     pushCommand(GCode::toCommand(GCode::MCommands::M220, QString::number(speed)));
 }
 
 void AtCore::setFlowRate(uint speed)
 {
-    speed = std::max<uint>(1, std::min<uint>(speed, 10000));
+    speed = std::max<uint>(0, std::min<uint>(speed, 10000));
     pushCommand(GCode::toCommand(GCode::MCommands::M221, QString::number(speed)));
 }
 
